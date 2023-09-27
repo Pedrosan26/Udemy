1878715a577f40de4726448d846daa68f8bc9491
Pedro San <pedrosava01@gmail.com>
2023-09-27 14:06:15 -0600

    pointers

--- Pointers/pointers.cpp
+++ Pointers/pointers.cpp
@@ -4,13 +4,13 @@
 
 int main(){
 
-    int a=10;
-    int *p;
+    int a=10; // a is an integer variable
+    int *p; // p is a pointer variable
 
-    p=&a;
+    p=&a; // p is pointing to the address of a
 
-    printf("%d",*p);
+    printf("%d",*p); // *p is the value of the address that p is pointing to
 
     return 0;
 
-}
+} // in this function we are not using the heap memory, we are using the stack memory
--- Pointers/pointersHeap.cpp
+++ Pointers/pointersHeap.cpp
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 
 int main(){
-    int *p; 
+    
     //Un puntero con arrays en C
     //p=(int *) /* Como malloc regresa un void pointer, entonces debeos de darle el tipo de puntero que sera*/malloc(5*sizeof(int)); //malloc accede al heap memory, pero debe de tener como argumento el tamano en bytes que utilizaras
 
     //alocar memoria del heap en c++
-    p=new int[5];
+    //int A[5]={1,2,3,4,5}; 
+    int *p;
+    // p=A;
+    p=new(int[5]);
+    p[0]=5;p[1]=10;p[2]=15;p[3]=20;p[4]=25;
 
+    for(int i=0;i<5;i++){
+        std::cout<<p[i]<<" ";
+    }
+
+    delete [ ]p;
+
+    return 0;
     
 } 
