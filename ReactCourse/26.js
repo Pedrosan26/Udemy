371d5cd2325cf43c23a1682ed9c97e15d80a7209
Pedro San <pedrosava01@gmail.com>
2023-11-10 12:01:06 -0600

    imports Exprots

---
+++ ReactCourse/26.js
@@ -0,0 +1,12 @@
+//imports y exports 
+
+sumar= (numero1, numero2) => numero1 + numero2;
+
+restar= (numero1, numero2) => numero1 - numero2;
+
+
+// se exporta como un objeto
+export{
+    sumar,
+    restar
+}
---
+++ ReactCourse/26_import.js
@@ -0,0 +1,10 @@
+//ocupasmos hacer un destructuring para poder usar las funciones
+
+import {sumar} from "./26.js"
+import {restar} from "./26.js"
+
+const resultado = sumar(10,20);
+console.log(resultado);
+
+const resultado2 = restar(10,20);
+console.log(resultado2);
