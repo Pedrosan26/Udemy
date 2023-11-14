ccaee607fc84b01e2878f02ebe32409b3fa33bb2
Pedro San <pedrosava01@gmail.com>
2023-11-14 12:18:37 -0600

    first component

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
+import Header from './components/header'
 
 function App() {
 
   return (
+    // con <> </> podmeos regresar multiples tags sin necesidad de crear muchos divs
+    // se regresa el elemento mas alto, poir lo que podemos usar <> </>
+    // para empezar codigo en JS debe de estar dentro de  {}
+  <>
+
     <div className='App'>
       <h1>Hola Mundo</h1>
     </div>
-    
+    <Header/>
+
+  </>
   )
 }
 
