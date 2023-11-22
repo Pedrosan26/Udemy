be40601e616e971394ca26381360c5ec47942f3b
Pedro Sanchez <pedrosava01@gmail.com>
2023-11-22 08:28:09 -0600

    Creacion de la lista de los pacientes nuevos

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Pacientes from './pacientes'
 
 const ListadoPacientes = () => {
   return (
-    <div className='w-1/2 lg:w-3/5'>
-        <h2>Listado Pacientes</h2>
+    <div className='w-1/2 lg:w-3/5 h-screen overflow-y-scroll'>
+        <h2 className='text-center text-3xl font-black'>Listado Pacientes</h2>
+        <p className='text-xl mt-5 mb-10 text-center'>
+          Administra tus {" "}
+          <span className=' text-indigo-600 font-black '> Pacientes y citas</span>
+        </p>
+        <Pacientes/>
+
     </div>
   )
 }
---
+++ citas-react/src/components/Pacientes.jsx
@@ -0,0 +1,35 @@
+
+
+const Pacientes = () => {
+  return (
+    <>
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+          <p className='font-bold mb-3 text-gray-700 uppercase'> Nombre 
+            <span className='font-normal normal-case'> Hook</span>
+          </p>
+        
+          <p className='font-bold mb-3 text-gray-700 uppercase'> Propietario 
+            <span className='font-normal normal-case'> Pedro San</span>
+          </p>
+        
+          <p className='font-bold mb-3 text-gray-700 uppercase'> Email 
+            <span className='font-normal normal-case'> dummy@hotmail.com</span>
+          </p>
+        
+    
+          <p className='font-bold mb-3 text-gray-700 uppercase'> Fecha 
+            <span className='font-normal normal-case'> 11/22/2023</span>
+          </p>
+        
+        
+          <p className='font-bold mb-3 text-gray-700 uppercase'> Sintomas 
+            <span className='font-normal normal-case'> Tiene mucha fiebre y no esta soportando </span>
+          </p>
+    </div>
+
+        </>
+  )
+  
+}
+
+export default Pacientes
